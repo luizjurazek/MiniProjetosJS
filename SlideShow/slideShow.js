686475f9c585c545f7c4b1323d2abe8a4b712993
luizjurazek <luizjurazek@gmail.com>
2021-08-06 13:44:53 -0300

    SlideShow

---
+++ SlideShow/slideShow.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const images = [
+    { 'id': '1', 'url':'./img/img1.jpg' },
+    { 'id': '2', 'url':'./img/img2.jpg' },
+    { 'id': '3', 'url':'./img/img3.jpg' },
+    { 'id': '4', 'url':'./img/img4.jpg' },
+    { 'id': '5', 'url':'./img/img5.jpg' },
+    { 'id': '6', 'url':'./img/img6.png' },
+]
+
+const containerItems = document.querySelector('#container-items');
+
+const loadImages = ( images, container ) => {
+    images.forEach ( image => {
+        container.innerHTML += `
+            <div class='item'>
+                <img src='${image.url}'
+            </div>
+        `
+    })
+}
+
+loadImages( images, containerItems );
+
+let items = document.querySelectorAll('.item');
+
+const previous = () => {
+    containerItems.appendChild(items[0]);
+    items = document.querySelectorAll('.item');
+}
+
+const next = () => {
+    const lastItem = items[items.length - 1];
+    containerItems.insertBefore( lastItem, items[0] );
+    items = document.querySelectorAll('.item');
+}
+
+document.querySelector('#previous').addEventListener('click', previous);
+document.querySelector('#next').addEventListener('click', next);
